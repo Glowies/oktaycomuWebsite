030c625921253d89adb727dd44178d7812f84b34
glowies <comuokta@gmail.com>
2019-04-15 11:17:28 -0400

    Removed RCNSm and updated RCNSu to the latest mobile version.

--- RCNSm/assets/RCmap/babylon/manifest.py
+++
@@ -1,8 +0,0 @@
-import os
-dir = raw_input("Directory:\t")
-for i in os.listdir("./"+dir):
-    if i.endswith(".babylon"):
-        print i
-        file = open(dir+"\\"+i+".manifest","w")
-        file.write("{\n\t'version': 1,\n\t'enableSceneOffline' : true,\n\t'enableTexturesOffline' : true\n}")
-        file.close()
