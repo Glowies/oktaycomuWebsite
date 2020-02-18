e48b1c530e230e140be7853a60879c692df0cb39
glowies <oktaycomu@gmail.com>
2020-02-17 19:58:17 -0500

    Privacy policy...

---
+++ privacyPolicy/index.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('../headImport.php'); ?>
+
+    <title>glowies - privacy policy</title>
+    <meta name="description" content="swift snake 3d privacy policy">
+</head>
+
+<body role="document" style="background-color: white; height:100%; -moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none; -o-user-select:none;">
+    <header>
+        <?php include_once('../navbar.php'); ?>
+    </header>
+    <main role="main">
+        <div class="container" align="center">
+            <div class="jumbotron jumbotron-fluid" align="left" style="background-image: url('assets/jumbotron_background.png'); background-repeat: no-repeat; background-position: right; background-size: contain">
+                <div class="container">
+                    <h1 class="display-4">Swift Snake 3D <br> Privacy Policy</h1>
+                    <p class="lead">Last Updated: February 17th, 2020</p>
+                </div>
+            </div>
+            <div align="left">
+                <h3>What Information Do I Collect?</h3>
+                I only collect the information that is essential to maintaining the live leaderboard.
+                This information consists of your email, nickname, and highest statistics (length, score, and combo).
+                Nothing else is stored in the databases I maintain.
+                Your email is acquired using the Google account that you use to log in.
+                There currently isn't any way of changing your email or transferring your data to another account.
+                You set your nickname when you first log in.
+                There currently isn't any way of changing your nickname.
+                The only way to alter your in game statistics is to achieve better statistics.
+                <br>
+                <br>
+
+                <h3>How do I use the information?</h3>
+                The information I collect is used for the sole purpose of recognizing your account and saving your scores.
+                All information is stored and automatically indexed on a MongoDB database.
+                No additional processing is done on the data.
+                The information is stored indefinitely on the database, unless a direct removal is requested by a user through email.
+                <br>
+                <br>
+
+                <h3>What information do I share?</h3>
+                None of the information I collect is shared with any third-party.
+                The only parties who can access the information is me, mLab (the service hosting the database), and Heroku (the service hosting the nodejs server).
+                <br>
+                <br>
+
+                <h3>Further inquiries or requests...</h3>
+                Contact me through <a href="mailto:6l0w135@gmail.com"><kbd><samp>6l0w135@gmail.com</samp></kbd></a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
